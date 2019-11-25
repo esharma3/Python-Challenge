2660a92e295319b6515c150c3561f2466a71d7a5
Ekta Sharma <ekta.sharma3@gmail.com>
2019-11-25 16:58:45 -0600

    Updated the code. Used DictReader to read the input file

--- PyPoll/main-PyPoll.py
+++ PyPoll/main-PyPoll.py
@@ -1,10 +1,8 @@
-
 import os
 import csv
 
 
 totalVotes = 0
-candidates = []
 candidate_voteCount = {}
 candidate_votePercent = {}
 
@@ -12,33 +10,30 @@ in_filepath = os.path.join('Input & Output', 'election_data.csv')
 out_filepath = os.path.join('Input & Output', 'election_data_analysis.csv')
 
 
-with open(in_filepath, 'r', newline = '') as in_text:
+with open(in_filepath, 'r', newline = '') as in_file:
 
-    reader = csv.reader(in_text, delimiter = ',')
-
-    header = next(reader)
-    
-    
-    # Calculating total votes casted and candidate list
-    for row in reader:
+    reader = csv.DictReader(in_file, delimiter = ',')
+    fieldnames = ['Voter ID', 'County', 'Candidate'] # Field Names for reference only
         
+    # Calculating total votes per candidate
+    for line in reader:
+                
         totalVotes = totalVotes + 1        
+        
+        if line['Candidate'] not in candidate_voteCount:
 
-        if row[2] not in candidates:
-
-            candidates.append(row[2])
-            candidate_voteCount[row[2]] = 1 
+            candidate_voteCount[line['Candidate']] = 1 
                          
-        elif row[2] in candidates:
+        else:
 
-            candidate_voteCount[row[2]] += 1
-
+            candidate_voteCount[line['Candidate']] += 1
+        
 
 # Calculating the Winner
 winner = max(candidate_voteCount, key=(lambda k: candidate_voteCount[k]))
  
 
-#Sorting the candidate list in descending order of number of candidate votes  
+# Sorting the candidate list in descending order of number of candidate votes  
 results_list = [[key,candidate_voteCount[key]] for key in candidate_voteCount]
 results_list = sorted(results_list, key=lambda row: row[1], reverse=True)
 
@@ -58,18 +53,16 @@ output2 = (f"--------------------------\n" f"Winner: {winner}\n" f"-------------
 # Printing output to the terminal
 print(output1)
 for key, value in candidate_votePercent.items():
-    print(key, ':', value)
+    print(key, ': ', value)
 print(output2)
 
 
 # Writing output to the file
-with open(out_filepath, 'w+', newline = '') as out_text:
+with open(out_filepath, 'w', newline = '') as out_file:
 
-    out_text.write((output1 + '\n'))
+    out_file.write((output1 + '\n'))
 
     for key, value in candidate_votePercent.items():
-        out_text.write((key + ':' + value + '\n'))
+        out_file.write((key + ': ' + value + '\n'))
 
-    out_text.write(output2)
-  
-   
+    out_file.write(output2)
